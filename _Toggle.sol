18477439403f58630f40fc6aa2be6d9175f5dfd0
dekasin <dekasins@gmail.com>
2023-07-04 11:55:41 +0300

    add: create functionality

--- _Toggle.sol
+++ _Toggle.sol
@@ -2,5 +2,14 @@
 pragma solidity 0.8.18;
 
 contract Toggle{
-    
+    // SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract Toggle{
+    bool  public flag;
+
+
+    function toggle ()public{
+        flag = !flag;
+    }
 }
