ed3213ba44ce20b17f6d36c126b9e5e7868c7b0d
dekasin <dekasins@gmail.com>
2023-07-04 12:00:02 +0300

    fix: add constructor, it should fix deploying

--- _Toggle.sol
+++ _Toggle.sol
@@ -8,6 +8,22 @@ pragma solidity 0.8.18;
 contract Toggle{
     bool  public flag;
 
+    // SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract Toggle{
+    bool  public flag;
+
+    constructor(){
+        flag = true;
+        }
+
+
+    function toggle ()public{
+        flag = !flag;
+    }
+}
+
 
     function toggle ()public{
         flag = !flag;
